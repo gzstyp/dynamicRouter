296f2e9edf9d9a495a968948af11171d8d304ffb
gzstyp <444141300@qq.com>
2020-08-09 00:45:55 +0800

    只是把名称取为懒加载!!!

--- src/utils/MenuUtils.js
+++ src/utils/MenuUtils.js
@@ -1,4 +1,4 @@
-import lazyLoading from './lazyLoading'; //即把字符串转为component组件
+import lazyLoading from './lazyLoading'; //即把字符串转为component组件,只是把名称取为懒加载!!!
 export default (routers,data) => {
   //这里之所以要重新遍历一下，是因为，通常我们动态路由的时候，是获取服务端数据，这个component属性是一个字符串，或者可能连字段名都是其他的key
   //所以这里要做一些转换
